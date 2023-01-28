68f4232b65136806bb34f9a7ee88f47eaca4e967
talalnuman1 <developertrisage1@gmail.com>
2023-01-28 19:19:17 +0500

    Profile

--- src/screens/profileScreen/index.js
+++ src/screens/profileScreen/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -18,41 +18,148 @@ import COLORS from '../../config/constant';
 import SelectDropdown from 'react-native-select-dropdown';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
+import {users} from '../../api';
+import {setUser} from '../../redux/user';
+import {useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Config from 'react-native-config';
+import Modal from 'react-native-modal';
 // import Profile from '../../assets/images/person-profile-image-icon.webp';
 // import { users, orders, wishlist, redeem, wallet, auth } from "../../api";
-
+const APPURL =
+  Config.REACT_APP_ENV === 'dev'
+    ? Config.REACT_APP_URL
+    : Config.REACT_APP_PRODUCTION_URL;
 const countriesWithFlags = [{title: 'EUR'}, {title: 'USD'}, {title: 'GBP'}];
 function ProfileScreen() {
+  const dispatch = useDispatch();
   const navigation = useNavigation();
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [currency, setCurrency] = useState('USD');
+  const [balance, setBalance] = useState(0);
+  const [token, setToken] = useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
+  const {user} = useSelector(state => state.user);
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
-  // const getUser = () => {
-  //   if (localStorage.getItem("token-access") !== null) {
-  //     const decoded = jwtDecode(localStorage.getItem("token-access"));
-  //     users(`/${decoded.sub}`, {
-  //       method: "get",
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem("token-access")}`,
-  //       },
-  //     })
-  //       .then((res) => {
-  //         // logger(res.data);
-  //         dispatch(setUser(res.data));
-  //         // dispatch(setLoginState(true));
-  //         // getCurrencyValue(res.data, currency);
-  //         // getRedeemables(res.data.id);
-  //         // logger(res.data.id, "after API Call");
+  const getToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token_access');
+      const user = JSON.parse(await AsyncStorage.getItem('user'));
+      console.log(user.id, 'uswreorder');
+      console.log(token, 'TOKEN');
+      getUserByID(token, user.id);
+      setToken(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  function getUserByID(token, userId) {
+    users(`/${userId}`, {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(res => {
+        dispatch(setUser(res.data));
+        getCurrencyValue(res.data, currency);
+        console.log(res.data);
+      })
+      .catch(error => {
+        // messageApi.error(error.response.data.message);
+        // dispatch(setLoginState(false));
+        console.log(error);
+      });
+  }
+  function addpoints() {
+    let add = user?.loyaltyPoints + user?.referralPoints;
+    return add;
+  }
+  function getCurrencyValue(user, coin) {
+    const value = user?.balance.map(currency => {
+      return currency[coin];
+    });
+    let data = value.filter(element => {
+      return element !== undefined;
+    });
+    setBalance(data[0]);
+  }
 
-  //         // messageApi.success("login success");
-  //       })
-  //       .catch((error) => {
-  //         // messageApi.error(error.response.data.message);
-  //         dispatch(setLoginState(false));
-  //         // logger(error.response.data.message);
-  //         logger(error);
-  //       });
-  //   }
-  // };
+  const onSHPRedeem = () => {
+    users(`${user.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token-access')}`,
+      },
+      data: {
+        loyaltyPoints: 0,
+        referralPoints: 0,
+      },
+    })
+      .then(response => {
+        console.log(response.data);
+        getUserByID(user.id, token);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const addBalance = coin => {
+    const updateBalance = () => {
+      let arr = [...user.balance];
+      console.log(arr);
+      arr.map((currency, index) => {
+        Object.entries(currency).find(([key, value]) => {
+          if (coin === key) {
+            arr[index] = {
+              [key]:
+                arr[index][key] +
+                (user.referralPoints + user.loyaltyPoints) / 100,
+            };
+            console.log(key, value);
+          }
+        });
+      });
+      return arr;
+    };
+    if (user.id) {
+      setRedeeming(true);
+      users(`${user.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token-access')}`,
+        },
+        data: {
+          balance: updateBalance(),
+        },
+      })
+        .then(response => {
+          logger(response.data, 'This is ADD Balance response');
+
+          onSHPRedeem();
+          message.success('SHP are added successfully are added in USD');
+          // createOrder(data);
+        })
+        .catch(error => {
+          logger(error);
+        })
+        .finally(() => {
+          setRedeeming(false);
+          handleBalanceCancel();
+        });
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView
@@ -66,9 +173,10 @@ function ProfileScreen() {
               <SelectDropdown
                 data={countriesWithFlags}
                 onSelect={(selectedItem, index) => {
-                  console.log(selectedItem, index);
+                  setCurrency(selectedItem.title);
+                  getCurrencyValue(user, selectedItem.title);
                 }}
-                defaultButtonText={'USD'}
+                defaultButtonText={currency}
                 buttonTextAfterSelection={(selectedItem, index) => {
                   return selectedItem.title;
                 }}
@@ -98,41 +206,43 @@ function ProfileScreen() {
             </View>
             <View style={styles.rowtext}>
               <Text style={styles.name}>Name:</Text>
-              <Text style={styles.greycolor}>Talal Numan</Text>
+              <Text style={styles.greycolor}>{user?.name}</Text>
             </View>
             <View style={styles.hairline1} />
             <View style={styles.rowtext}>
               <Text style={styles.name}>Email:</Text>
-              <Text style={styles.greycolor}>talal@gmail.com</Text>
+              <Text style={styles.greycolor}>{user?.email}</Text>
             </View>
             <View style={styles.hairline1} />
             <View style={styles.rowtext}>
               <Text style={styles.name}>Account Balance:</Text>
-              <Text style={styles.greycolor}>5.33 USD</Text>
+              <Text style={styles.greycolor}>
+                {balance.toFixed(2)} {currency}
+              </Text>
             </View>
             <View style={styles.hairline1} />
             <View style={styles.rowtext}>
               <Text style={styles.name}>Loyalty Points:</Text>
-              <Text style={styles.greycolor}>0.50 SHP</Text>
+              <Text style={styles.greycolor}>{user?.loyaltyPoints} SHP</Text>
             </View>
             <View style={styles.hairline1} />
             <View style={styles.rowtext}>
               <Text style={styles.name}>Referral Points:</Text>
-              <Text style={styles.greycolor}>0 SHP</Text>
+              <Text style={styles.greycolor}>{user?.referralPoints} SHP</Text>
             </View>
             <View style={styles.hairline1} />
             <View style={styles.rowtext}>
               <Text style={styles.name}>Total SHP:</Text>
-              <Text style={styles.greycolor}>0.50 SHP</Text>
+              <Text style={styles.greycolor}>{addpoints()} SHP</Text>
             </View>
             <View>
-              <TouchableOpacity style={styles.redbtncopy}>
+              <TouchableOpacity style={styles.redbtncopy} onPress={toggleModal}>
                 <Text style={styles.colorwhite}>Redeem SHP</Text>
               </TouchableOpacity>
             </View>
             <Text style={styles.name1}>Referral Code:</Text>
             <Text style={styles.greycolor1}>
-              https://switchive.vercel.app /signup/63b7f9221562170d98533ba6
+              {APPURL}/signup/63b7f9221562170d98533ba6
             </Text>
             <View>
               <TouchableOpacity style={styles.redbtncopy}>
@@ -142,12 +252,24 @@ function ProfileScreen() {
           </View>
         </LinearGradient>
       </ScrollView>
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.container22}>
+          <TouchableOpacity onPress={toggleModal} style={styles.modelbtn23}>
+            <Text style={styles.greycolor1}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  container22: {
+    paddingHorizontal: wp('4'),
+    paddingVertical: hp('1'),
+    backgroundColor: 'white',
   },
   linearGradient: {
     paddingHorizontal: wp('2'),
